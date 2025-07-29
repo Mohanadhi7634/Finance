3baaf34ae129dd643133c1b6b2ed54d4b7be8e37
Mohanadhi7634 <137165062+Mohanadhi7634@users.noreply.github.com>
2025-07-29 16:36:06 +0530

    server.js

--- server.js
+++ server.js
@@ -8,10 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: ["https://your-frontend-url.com"],
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
