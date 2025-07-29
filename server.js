cff5ab1d973a5c5629a12e9e4a69445a81cbcc24
Mohanadhi7634 <137165062+Mohanadhi7634@users.noreply.github.com>
2025-07-29 16:27:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["https://your-frontend-url.com"],
+  credentials: true
+}));
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 // app.use(bodyParser.json({ limit: "50mb" }));
